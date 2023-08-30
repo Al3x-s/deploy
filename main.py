3432aa9214b15578096b112e171edbb963f553bc
Alejandro Sanchez <33365377+Al3x-s@users.noreply.github.com>
2023-08-30 11:11:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -60,6 +60,6 @@ def home():
         return render_template("index.html", user_data = user_data, stat=has_entered)
     else:
         return redirect(url_for('login'))
-
-
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
 
